e0e853958cd7dbdf79eee59a85549b7482459ef0
15719062 <>
2021-06-06 15:51:59 +0000

    I added the function "create_line_geom".

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -31,15 +31,15 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
-def create_line_geom(Point1,Point2):
- line = LineString([Point1,Point2])
-
+def create_line_geom(points):
+ line = LineString(points)
  return line
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
 line1 = None
 # YOUR CODE HERE 3 to define two points and store the result in line1
-line1 = create_line_geom([Point(45.2, 22.34),Point(100.22, -3.20)])
+points=[Point(45.2, 22.34),Point(100.22, -3.20)]
+line1 = create_line_geom(points)
 
 # CODE FOR TESTING YOUR SOLUTION
 print(line1)
@@ -70,12 +70,15 @@ except Exception as e:
 
 
 # YOUR CODE HERE 4 to define create_poly_geom()
+def create_poly_geom(coords):
+ poly=Polygon(coords)
 
+ return poly
 # Demonstrate the usage of the function. For example, create a Polygon with three points: `(45.2, 22.34)`, `(100.22, -3.20)` & `(70.0, 10.20)`.
 
 # YOUR CODE HERE 5 to define poly1 with three points
-
-
+coords=[(45.2, 22.34),(100.22, -3.20),(70.0, 10.20)]
+poly1=create_poly_geom(coords)
 # CODE FOR TESTING YOUR SOLUTION
 print(poly1)
 
